2ea7a87098a8e30e06038386ee60cba8e1748548
vits56 <bastosduartebruno@gmail.com>
2022-09-28 17:46:43 -0300

    Initial commit

---
+++ src/main/java/com/vits56/restauranteapi/RestauranteapiApplication.java
@@ -0,0 +1,13 @@
+package com.vits56.restauranteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauranteapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vits56/restauranteapi/RestauranteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vits56.restauranteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
