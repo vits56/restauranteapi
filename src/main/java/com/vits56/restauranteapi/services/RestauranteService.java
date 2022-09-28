01eaf7c14f7f605721f73a24aaa093c6a333e9d0
vits56 <bastosduartebruno@gmail.com>
2022-09-28 18:05:57 -0300

    classe RestauranteService

---
+++ src/main/java/com/vits56/restauranteapi/services/RestauranteService.java
@@ -0,0 +1,14 @@
+package com.vits56.restauranteapi.services;
+
+import com.vits56.restauranteapi.entities.Restaurante;
+import com.vits56.restauranteapi.services.dto.RestauranteDto;
+
+import java.util.List;
+
+public interface RestauranteService {
+    Restaurante incluir(RestauranteDto form);
+    List<Restaurante> buscarTodos();
+    Restaurante buscarPorId(Long id);
+    Restaurante atualizar(Long id, RestauranteDto formAtualizar);
+    void deletar(Long id);
+}
